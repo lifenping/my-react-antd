1b8e61de3c8287654314d85500fb4efbe8b2297a
李粉萍 <fpli@corp.netease.com>
2018-12-05 15:02:50 +0800

    [表格行扩展属性]测试

--- src/pages/react/antd/TableExpand.js
+++ src/pages/react/antd/TableExpand.js
@@ -4,12 +4,26 @@
  */
 import React, { Component } from 'react';
 import { Table } from 'antd';
+import style from './index.less';
+import { Button } from 'antd/lib/radio';
+const columns = [
+	{ title: 'Name', dataIndex: 'name', key: 'name' },
+	{ title: 'Age', dataIndex: 'age', key: 'age' },
+	{ title: 'Address', dataIndex: 'address', key: 'address' },
+	{ title: 'Action', dataIndex: '', key: 'x', render: () => <a href="#">Delete</a> },
+];
 
+const data = [
+	{ key: 1, name: 'John Brown', age: 32, address: 'New York No. 1 Lake Park' },
+	{ key: 2, name: 'Jim Green', age: 42, address: 'London No. 1 Lake Park', description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.' },
+	{ key: 3, name: 'Joe Black', age: 32, address: 'Sidney No. 1 Lake Park', description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.' },
+];
 export default class Grid extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			expandedRowKeys: [1, 2, 3]
+			expandedRowKeys: [1, 2, 3],
+			columns: columns,
 		}
 	}
 	expandedRowRender = (record) => {
@@ -35,34 +49,43 @@ export default class Grid extends Component {
 			})
 		}
 	}
+	hiddenFirstCollumns = () => {
+		const { columns } = this.state;
+		columns[0].className = 'hidden';
+		this.setState({
+			columns: columns
+		})
+	}
+
+	showFirstCollumns = () => {
+		const { columns } = this.state;
+		columns[0].className = '';
+		this.setState({
+			columns: columns
+		})
+	}
 
 	render() {
-		const columns = [
-			{ title: 'Name', dataIndex: 'name', key: 'name' },
-			{ title: 'Age', dataIndex: 'age', key: 'age' },
-			{ title: 'Address', dataIndex: 'address', key: 'address' },
-			{ title: 'Action', dataIndex: '', key: 'x', render: () => <a href="#">Delete</a> },
-		];
-
-		const data = [
-			{ key: 1, name: 'John Brown', age: 32, address: 'New York No. 1 Lake Park' },
-			{ key: 2, name: 'Jim Green', age: 42, address: 'London No. 1 Lake Park', description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.' },
-			{ key: 3, name: 'Joe Black', age: 32, address: 'Sidney No. 1 Lake Park', description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.' },
-		];
-
 		return (
-			<Table
-				columns={columns}
-				expandedRowRender={record => this.expandedRowRender(record)}
-				dataSource={data}
-				// defaultExpandAllRows={true}
-				expandedRowKeys={this.state.expandedRowKeys}
-				onExpand={(expanded, record) => this.onExpand(expanded, record)}
+			<div>
+				<p>
+					<Button onClick={this.hiddenFirstCollumns}>隐藏第一列</Button>
+					<Button onClick={this.showFirstCollumns}>显示第一列</Button>
+				</p>
+				<p>通过给collumns添加className来隐藏列，样式内容为 display:none; 显示时调为:className=''</p>
+				<Table
+					columns={this.state.columns}
+					expandedRowRender={record => this.expandedRowRender(record)}
+					dataSource={data}
+					// defaultExpandAllRows={true}
+					expandedRowKeys={this.state.expandedRowKeys}
+					onExpand={(expanded, record) => this.onExpand(expanded, record)}
 				// 使控制子行显示与隐藏的按钮不在新追加的列中显示，而是在第一个td中显示
 				// expandIconAsCell={false}
 				// 控制收缩与隐藏子行的icon在哪列显示，值为number，当值为null时，不显示此icon
-				// expandIconColumnIndex={null}
-			/>
+				// expandIconColumnIndex={2}
+				/>
+			</div>
 		)
 	}
 }
